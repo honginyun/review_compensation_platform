1f4085d4d5c2fac5a8a79be3df29ef81c9cf441b
Raghu Betina <raghu@firstdraft.com>
2019-06-06 22:46:59 +0000

    Whitelist columns in VoteComment for ActiveAdmin

--- app/admin/vote_comments.rb
+++ app/admin/vote_comments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register VoteComment do
+
+ permit_params :voter_id, :comment_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
