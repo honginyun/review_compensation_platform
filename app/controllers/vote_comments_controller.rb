1fe8da39d0ebb7cab0b24033382a38746296d194
Raghu Betina <raghu@firstdraft.com>
2019-06-06 22:47:29 +0000

    Only vote_comment user can modify vote_comment

--- app/controllers/vote_comments_controller.rb
+++ app/controllers/vote_comments_controller.rb
@@ -1,4 +1,14 @@
 class VoteCommentsController < ApplicationController
+  before_action :current_user_must_be_vote_comment_voter, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_vote_comment_voter
+    vote_comment = VoteComment.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == vote_comment.voter
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @vote_comments = VoteComment.all
 
